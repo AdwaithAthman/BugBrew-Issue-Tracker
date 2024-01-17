31936b1e2eb4174c739ef598525e058e14992101
AdwaithAthman <iamadwaith17@gmail.com>
2024-01-17 10:16:19 +0530

    Add the login and logout links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,9 +5,12 @@ import Link from "next/link";
 import { AiFillBug } from "react-icons/ai";
 import { usePathname } from "next/navigation";
 import classnames from "classnames";
+import { useSession } from "next-auth/react";
+import { Box } from "@radix-ui/themes";
 
 const NavBar = () => {
   const currentPath = usePathname();
+  const { status, data: session } = useSession();
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues/list" },
@@ -19,19 +22,28 @@ const NavBar = () => {
       </Link>
       <ul className="flex space-x-6">
         {links.map((link) => (
-          <Link
-            key={link.label}
-            href={link.href}
-            className={classnames(
-              { "text-zinc-900": link.href === currentPath },
-              { "text-zinc-500": link.href !== currentPath },
-              "hover:text-zinc-800 transition-colors"
-            )}
-          >
-            {link.label}
-          </Link>
+          <li key={link.label}>
+            <Link
+              href={link.href}
+              className={classnames(
+                { "text-zinc-900": link.href === currentPath },
+                { "text-zinc-500": link.href !== currentPath },
+                "hover:text-zinc-800 transition-colors"
+              )}
+            >
+              {link.label}
+            </Link>
+          </li>
         ))}
       </ul>
+      <Box>
+        {status === "authenticated" && (
+          <Link href="/api/auth/signout">Logout</Link>
+        )}
+        {status === "unauthenticated" && (
+          <Link href="/api/auth/signin">Login</Link>
+        )}
+      </Box>
     </nav>
   );
 };
