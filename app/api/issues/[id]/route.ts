9065d72a71bcbee5ae2c27e0b64a25fa34840207
AdwaithAthman <iamadwaith17@gmail.com>
2024-01-20 17:56:03 +0530

    Resolved bugs while building

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -2,7 +2,7 @@ import { patchIssueSchema } from "@/app/validationSchemas";
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../../auth/[...nextauth]/route";
+import authOptions from "@/app/auth/authOptions";
 
 export async function PATCH(
   request: NextRequest,
--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
 import { issueSchema } from "@/app/validationSchemas";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/route";
+import authOptions from "@/app/auth/authOptions";
 
 export async function POST(request: NextRequest) {
     const session = await getServerSession(authOptions);
