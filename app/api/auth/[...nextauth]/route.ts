f6e9e62ba9222fcdae39e5b44b5db0d81365056f
AdwaithAthman <iamadwaith17@gmail.com>
2024-01-20 17:38:46 +0530

    Resolved authOptions bug by moving to auth folder

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,19 +1,6 @@
-import NextAuth, { NextAuthOptions, SessionStrategy } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "@/prisma/client";
+import authOptions from "@/app/auth/authOptions";
+import NextAuth from "next-auth"
 
-export const authOptions: NextAuthOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-        })
-    ],
-    session: {
-        strategy: "jwt" as SessionStrategy
-    }
-}
 const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST}
+
+export { handler as GET, handler as POST }
